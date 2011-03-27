5c7b927e84faf5236bac9737a139ed5b562e124a
Markus Arike <markus.arike@gmail.com>
2011-03-27 19:32:43 -0400

    end of chapter exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round shadowed-box")
+  end
+  
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
